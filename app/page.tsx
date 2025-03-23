8d2dfda8697fe6841310ffe717bbbfa9d5775364
Sampilyc <167899192+Sampilyc@users.noreply.github.com>
2025-03-23 19:27:26 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import InteractiveAvatar from "@/components/InteractiveAvatar";
 
 export default function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-black">
+    <div className="w-screen h-screen bg-black flex items-center justify-center p-0 m-0">
       <div className="w-full max-w-[100vh] aspect-[9/16]">
         <InteractiveAvatar />
       </div>
